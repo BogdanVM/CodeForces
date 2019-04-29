4fdd780761ebcd401dec528a25c86187bf35057a
Bogdan Vilculescu <bogdanvm@192.168.1.9>
2019-04-29 15:43:57 +0300

    Updated Round555/main.cpp

--- CodeForces Round #555/C2 - Increasing Subsequence (hard version)/main.cpp
+++ CodeForces Round #555/C2 - Increasing Subsequence (hard version)/main.cpp
@@ -4,15 +4,37 @@
 
 using namespace std;
 
+/**
+ * Finds the number that breaks the strictly increasing sequence starting from the specified position.
+ * @param v vector of ints
+ * @param start start index from where to search
+ * @param right if true => the search will be done from the right side of the array
+ * @return index of the element that breaks the strictly increasing sequence
+ */
+int findPivotPosition(const vector<int>& v, int start, bool right = false) {
+    while (true) {
+        if (right) {
+            if (v[start] >= v[start-1]) {
+                return start-1;
+            }
+            start--;
+        } else {
+            if (v[start] >= v[start+1]) {
+                return start+1;
+            }
+            start++;
+        }
+    }
+}
+
 int main()
 {
     int n;
     cin >> n;
 
-    vector<pair<int, char>> v(n);
+    vector<int> v(n);
     for (int i = 0; i < n; ++i) {
-        cin >> v[i].first;
-        v[i].second = 'N';
+        cin >> v[i];
     }
 
     if (n == 1) {
@@ -25,50 +47,39 @@ int main()
     int right = n-1;
     string sequence;
 
-    v[0].second = 'L';
-    v[n-1].second = 'R';
     while (left <= right) {
-        if (currentNumber < v[left].first && currentNumber < v[right].first) {
-            if (v[left].first < v[right].first) {
-                sequence.push_back(v[left].second);
-                currentNumber = v[left].first;
-                left++;
+        if (currentNumber < v[left] && currentNumber < v[right]) {
+            if (v[left] < v[right]) {
+                sequence.push_back('L');
+                currentNumber = v[left++];
+            } else if (v[right] < v[left]){
+                sequence.push_back('R');
+                currentNumber = v[right--];
+            } else if (v[right] == v[left]){
+                int posRight = findPivotPosition(v, right, true);
+                int posLeft = findPivotPosition(v, left);
 
-                if (v[left].second == 'N') {
-                    v[left].second = 'L';
-                }
-            } else {
-                sequence.push_back(v[right].second);
-                currentNumber = v[right].first;
-                right--;
+                /* Numbers added to the sequence if we choose the right or the left side */
+                int addedRight = right - posRight;
+                int addedLeft = posLeft - left;
 
-                if (right >= 0) {
-                    if (v[right].second == 'N') {
-                        v[right].second = 'R';
-                    }
+                if (addedLeft > addedRight) {
+                    sequence.push_back('L');
+                    currentNumber = v[left++];
+                } else {
+                    sequence.push_back('R');
+                    currentNumber = v[right--];
                 }
             }
         } else {
-            if (currentNumber < v[left].first) {
-                sequence.push_back(v[left].second);
-                currentNumber = v[left].first;
-                left++;
-            } else if (currentNumber < v[right].first) {
-                sequence.push_back(v[right].second);
-                currentNumber = v[right].first;
-                right--;
+            if (currentNumber < v[left]) {
+                sequence.push_back('L');
+                currentNumber = v[left++];
+            } else if (currentNumber < v[right]) {
+                sequence.push_back('R');
+                currentNumber = v[right--];
             } else {
                 break;
-            }
-
-            if (v[left].second == 'N') {
-                v[left].second = 'L';
-            }
-
-            if (right >= 0) {
-                if (v[right].second == 'N') {
-                    v[right].second = 'R';
-                }
             }
         }
     }
